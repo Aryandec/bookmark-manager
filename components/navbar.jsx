25b129cfcb4cbca1c7cf88f6fcb7dee2ce37d0a5
Aryandec <aryanbh480@gmail.com>
2025-06-05 15:55:19 +0530

    initial navbar

---
+++ components/navbar.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FaBookBookmark } from "react-icons/fa6";
+import NavItems from "./navitems";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between mx-auto w-full bg-white px-6 py-4 shadow-md">
+      <Link href="/">
+        <div className="flex items-center gap-3 cursor-pointer">
+          <FaBookBookmark className="text-2xl text-blue-600" />
+          <span className="text-xl font-bold text-gray-800">Bookmark Manager</span>
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <p className="text-gray-600 hover:text-blue-600 cursor-pointer">Sign in</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
